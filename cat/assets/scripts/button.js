696beeb344c6b29a7848a2ee296b4706155a6aa2
赵俊涛 <534239589@qq.com>
2018-12-27 21:27:16 +0800

    complete 1st module

--- cat/assets/scripts/button.js
+++ cat/assets/scripts/button.js
@@ -8,9 +8,12 @@
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
 
+var bag_length = 10
 cc.Class({
     extends: cc.Component,
-
+    editor: {
+        requireComponent: sp.Skeleton
+    },
 
 
     properties: {
@@ -30,23 +33,75 @@ cc.Class({
         //     }
         // },
 
+        cat:{
+            type: cc.Node,
+            default:null,
+        },
+
+        opt_prefab:{
+            type:cc.Prefab,
+            default: null,
+        },
+
+        scrollView:{
+            type:cc.ScrollView,
+            default:null,
+        }
+
 
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        // 隐藏
+        this.scrollView.node.active = false;
+        // 生成默认
+        this.generateDefault();
+        // 生成包内 id
+        this.setSkin()
 
+
+        
     }, 
 
-    start () {
-
+    generateDefault(){
+        this.cat.getComponent('cat').skin_id = [1,1,1,1,2,1,1,2,1,1,1]
+        this.cat.getComponent('cat').refresh()
     },
+
+    setSkin(){
+        // var bag = this.bag = []
+        // 生成包内内容
+        for(var i =0; i<bag_length;i++){
+            var opt_item = cc.instantiate(this.opt_prefab);
+            
+            let target = opt_item.getChildByName('cat')
+            target.getComponent('cat').skin_id=[1,1,1,1,2,2,1,2,1,2,1];
+            this.scrollView.content.addChild(opt_item)
+        }
+    },
+
+    refreshSkin(){
+        let local = this.scrollView.content.getChildren()
+        for(var i =0; i<local.length;i++){
+            let opt_item = local[i]
+            opt_item.getChildByName('cat').getComponent('cat').refresh()
+            
+        }   
+    },
+
+    start () {
+        // 更新包内皮肤
+         
+    },
+
+
 
 
     // update (dt) {},
 
-    btnClick1: function (event, customEventData) {
+    alert: function (event, customEventData) {
         // //这里 event 是一个 Touch Event 对象，你可以通过 event.target 取到事件的发送节点
         // var node = event.target;
         // var button = node.getComponent(cc.Button);
@@ -55,6 +110,15 @@ cc.Class({
         Alert.show("开始养猫🐎",confirm(), true);
 
         
+    },
+
+    closeBag: function(event, customEventData) {
+        this.scrollView.node.active = false
+    },
+    
+    openBag: function (event, customEventData) {
+        this.scrollView.node.active = true
+        this.refreshSkin()
     },
 
     confirm: function(){
--- cat/assets/scripts/cat.js
+++ cat/assets/scripts/cat.js
@@ -7,7 +7,8 @@
 // Learn life-cycle callbacks:
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
-var skin_id = [1,1,1,1,1,1,1,1,1,1] 
+// var skin_id = [1,1,1,1,1,1,1,1,1,1]
+var parts = ['face','body','ear-l','ear-r','tail','eye-l','eye-r','hair','mos-l','mos-r','mouse']
 cc.Class({
     extends: cc.Component,
 
@@ -31,60 +32,46 @@ cc.Class({
         //         this._bar = value;
         //     }
         // },
+
+        skin_id:{
+            type: Array,
+            default:[]
+        }
         
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        var spine = this.spine = this.getComponent('sp.Skeleton');
+        // 赋值
+        // this.skin_id = [1,1,1,1,1,1,1,1,1,1,1]
+        // this.refresh()
+       
         
-        // set skin
-        let ear_l = this.spine.findSlot('ear-l')
-        let atta_ear_l = this.spine.getAttachment('ear-l',skin_id[2])
-        ear_l.setAttachment(atta_ear_l)
+    },
 
-        let ear_r = this.spine.findSlot('ear-r')
-        let atta_ear_r = this.spine.getAttachment('ear-r',skin_id[3])
-        ear_r.setAttachment(atta_ear_r)
+    refresh(){
+        var spine = this.spine = this.getComponent('sp.Skeleton');
 
+        for(var i=0;i<parts.length;i++){
+            console.log(parts[i],this.skin_id[i])
+            this.loadCat(parts[i],this.skin_id[i])
+        }
+        console.log('生成完毕')
+    },
 
-        let tail = this.spine.findSlot('tail')
-        let atta_tail = this.spine.getAttachment('tail',skin_id[4])
-        tail.setAttachment(atta_tail)
+    loadCat: function(part, id){
 
-        let eye_l = this.spine.findSlot('eye-l')
-        let atta_eye_l = this.spine.getAttachment('eye-l',skin_id[5])
-        eye_l.setAttachment(atta_eye_l)
-
-        let eye_r = this.spine.findSlot('eye-r')
-        let atta_eye_r = this.spine.getAttachment('eye-r',skin_id[6])
-        eye_r.setAttachment(atta_eye_r)
-
-        let hair = this.spine.findSlot('hair')
-        let atta_hair = this.spine.getAttachment('hair',skin_id[7])
-        hair.setAttachment(atta_hair)
-
-        let mos_l = this.spine.findSlot('mos-l')
-        let atta_mos_l = this.spine.getAttachment('mos-l',skin_id[8])
-        mos_l.setAttachment(atta_mos_l)
-
-        let mos_r = this.spine.findSlot('mos-r')
-        let atta_mos_r = this.spine.getAttachment('mos-r',skin_id[9])
-        mos_r.setAttachment(atta_mos_r)
-
-        let mouse = this.spine.findSlot('mouse')
-        let atta_mouse = this.spine.getAttachment('mouse',skin_id[9])
-        mouse.setAttachment(atta_mouse)
-
-
-
-
+        let slot = this.spine.findSlot(part)
+        let atta = this.spine.getAttachment(part,id)
+        slot.setAttachment(atta)
     },
 
     start () {
 
     },
 
-    // update (dt) {},
+    // update (dt) {
+        
+    // },
 });
