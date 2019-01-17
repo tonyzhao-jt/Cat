cef6ea41ddf3e3a867e9c283f1662c95a3d989a7
赵俊涛 <534239589@qq.com>
2019-01-17 17:47:18 +0800

    push

--- cat-backend/GenesInter.sol
+++
@@ -1,6 +0,0 @@
-contract GenesInter {
-
-    function isGenes() public pure returns (bool);
-
-    function mixGenes(uint256 genes1, uint256 genes2, uint256 targetBlock) public returns (uint256);
-}
